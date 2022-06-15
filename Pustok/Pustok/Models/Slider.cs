e835127b03af922516ddf1ed052233ac2e4e51ba
lamie2 <7metelt@code.edu.az>
2022-06-15 15:18:58 +0400

    first commit

---
+++ Pustok/Pustok/Models/Slider.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pustok.Models
+{
+    public class Slider
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(maximumLength: 35)]
+        public string Title1 { get; set; }
+        [StringLength(maximumLength: 35)]
+        public string Title2 { get; set; }
+        [StringLength(maximumLength: 250)]
+        public string Desc { get; set; }
+        [StringLength(maximumLength: 35)]
+        public string BtnText { get; set; }
+        [StringLength(maximumLength: 250)]
+        public string BtnUrl { get; set; }
+        [StringLength(maximumLength: 100)]
+        public string Image { get; set; }
+        [NotMapped]
+        public IFormFile ImageFile { get; set; }
+      
+    }
+}
