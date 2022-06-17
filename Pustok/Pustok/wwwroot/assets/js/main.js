c4a4ad7670cf15f14d41f93c758995a0ff542085
Lamie2 <7metelt@code.edu.az>
2022-06-17 12:52:57 +0400

    changes

---
+++ Pustok/Pustok/wwwroot/assets/js/main.js
@@ -0,0 +1,24 @@
+﻿$(document).ready(function () {
+
+    $(".show-detail").click(function (e) {
+        e.preventDefault();
+        let url = $(this).attr("href");
+
+        fetch(url).then(response => {
+            if (!response.ok) {
+                alert("Xeta bash verdi")
+                return;
+            }
+            return response.text()
+        })
+            .then(data => {
+                if (data) {
+                    $("#bookDetailModal .modal-content").html(data);
+                    $("#bookDetailModal").modal('show');
+                }
+
+            })
+
+
+    })
+})
